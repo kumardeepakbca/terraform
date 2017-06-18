20f17c249f68147cf4aef1ce370b56453cb71eae
pavan <paa1.metla@gmail.com>
2017-06-18 13:01:44 +0530

    sample

---
+++ sample.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "AKIAJELYSTZFZCSVV4IA"
+  secret_key = "+ewNOPTtcqAMTQRt7gpVI+fmkJyQfFdFDl0LaVAX"
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "sample" {
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+  }
+  
+ output "ip" {
+    value = "${aws_instance.sample.public_ip}"
+}
